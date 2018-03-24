4e65fc5e7dedcd7ed35e8003d9c8557d3dc9f82a
Welch Canavan <welch.canavan@gmail.com>
2018-03-24 18:59:00 -0400

    templates and routing

---
+++ app/routes/songs.js
@@ -0,0 +1,32 @@
+import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+let Song = EmberObject.extend({
+  title: "",
+  band: "",
+  rating: 0
+});
+
+export default Route.extend({
+  model() {
+    let blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+
+    let yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    let pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    return [blackDog, yellowLedbetter, pretender];
+  }
+});
